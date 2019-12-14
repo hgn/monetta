de92caae131881f94a850e27594ff68c803ca780
Hagen Paul Pfeifer <hagen@jauu.net>
2019-12-14 15:45:41 +0100

    fs: add basic func
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- httpd/rest/fs.py
+++ httpd/rest/fs.py
@@ -4,5 +4,27 @@
 import os
 import sys
 import asyncio
+import asyncio
+import concurrent.futures
 
 
+def blocking_io():
+    # File operations (such as logging) can block the
+    # event loop: run them in a thread pool.
+    with open('/dev/urandom', 'rb') as f:
+        return f.read(100)
+
+async def main():
+    loop = asyncio.get_running_loop()
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        result = await loop.run_in_executor(
+            pool, blocking_io)
+        print('custom thread pool', result)
+
+
+async def handle(request):
+    root = request.app['path-root']
+    full = os.path.join(root, "httpd/assets/webpage/irq.html")
+    with open(full, 'r') as content_file:
+        content = str.encode(content_file.read())
+        return web.Response(body=content, content_type='text/html')
