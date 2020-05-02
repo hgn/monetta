e43ffabc345f49bcb14ef6b59f98ae883c4ad83c
Hagen Paul Pfeifer <hagen@jauu.net>
2020-05-02 12:26:44 +0200

    fs: re-work FS access
    Signed-off-by: Hagen Paul Pfeifer <hagen@jauu.net>

--- httpd/assets/js/fs.js
+++ httpd/assets/js/fs.js
@@ -7,15 +7,35 @@ async function RestGetRootDir(name)
   return data;
 }
 
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function FileQuery(path)
+{
+	let url = '/api/v1/fs?mode=files&path=' + path;
+	try {
+		let response = await fetch(url);
+		let data = await response.json()
+		return data;
+	}
+	catch(err) {
+		return [];
+	}
+}
+
+
 function RestQueryDirectory(root_directory)
 {
-	let url = '/api/v1/fs?mode=file-listing&path=' + root_directory;
+	let url = '/api/v1/fs?mode=filesg&path=' + root_directory;
   fetch(url)
 		.then(function(response) {
 			return response.json()
 		})
 		.then(data => {
-			updateFSTable(root_directory, data);
+			if (data) {
+				updateFSTable(root_directory, data);
+			}
 		})
 		.catch(function(error) {
 			console.log('Request failed', error)
@@ -41,17 +61,17 @@ function tableHeader(root_directory)
 function tableContent(data)
 {
 	var str = "";
-	data['files'].forEach(function(entry) {
 		str += '<tr>';
-		str += '<td>' + entry['path'] + '</td>';
+		str += '<td>' + data['path'] + '</td>';
+		/*
 		str += '<td>' + entry['name'] + '</td>';
 		str += '<td>' + entry['size'] + '</td>';
 		str += '<td>' + entry['user'] + '</td>';
 		str += '<td>' + entry['group'] + '</td>';
 		str += '<td>' + entry['mode'] + '</td>';
 		str += '<td>' + entry['mtime'] + '</td>';
+		*/
 		str += '</tr>';
-	});
 	return str;
 }
 
@@ -70,21 +90,23 @@ function updateFSTable(root_directory, data)
 {
 	let entry;
 
-	entry  = tableHeader(root_directory);
 	entry += tableContent(data);
-	entry += tableFooter();
 	tableInsert(entry);
 }
 
-function ProcessRootDirData(data)
+function ProcessFileQuery(data)
 {
-	let directories = data['directories'];
-	directories.forEach(function(entry) {
-		RestQueryDirectory(entry);
+	data.forEach(function(entry) {
+		if (entry.type == 'directory') {
+			FileQuery(entry.path).then(data => ProcessFileQuery(data));
+		} else {
+			updateFSTable('/', entry);
+		}
+		console.log(entry);
 	});
 }
 
 $(document).ready(function() {
-	RestGetRootDir().then(data => ProcessRootDirData(data));
+	FileQuery('/').then(data => ProcessFileQuery(data));
 });
 
--- httpd/rest/fs.py
+++ httpd/rest/fs.py
@@ -90,6 +90,8 @@ def handle_fs(request):
         return handle_file_listing(request)
     elif request.rel_url.query['mode'] == "root-dir":
         return handle_root_dirs(request)
+    elif request.rel_url.query['mode'] == "files":
+        return handle_listing(request)
     return web.HTTPBadRequest(text=gen_err_msg)
 
 
@@ -104,6 +106,35 @@ def handle_root_dirs(request):
             db['directories'].append(path)
         else:
             db['files'].append(path)
+    return web.json_response(db)
+
+
+def handle_listing(request):
+    if 'path' not in request.rel_url.query:
+        return web.HTTPBadRequest(text='path must start with slash')
+    path = str(request.rel_url.query['path'])
+
+    db = list()
+    if path.startswith('/proc'):
+        return web.HTTPBadRequest(text='/proc not supported')
+    if path.startswith('/sys'):
+        return web.HTTPBadRequest(text='/sys not supported')
+    try:
+        files = os.listdir(path)
+    except FileNotFoundError:
+        # FIXME: return empty list, probalby return
+        # something else
+        return web.HTTPBadRequest(text='file not found error')
+    for _file in sorted(files):
+        entry = dict()
+        entry['path'] = os.path.join(path, _file)
+        if os.path.isdir(entry['path']):
+            entry['type'] = 'directory'
+        else:
+            entry['type'] = 'file'
+        db.append(entry)
+    if len(db) < 1:
+        return web.HTTPBadRequest(text='no data')
     return web.json_response(db)
 
 
@@ -141,6 +172,4 @@ def handle_file_listing(request):
 
 async def handle(request):
     # root = request.app['path-root']
-    loop = asyncio.get_running_loop()
-    with concurrent.futures.ThreadPoolExecutor() as pool:
-        return await loop.run_in_executor(pool, handle_fs, request)
+    return handle_fs(request)
